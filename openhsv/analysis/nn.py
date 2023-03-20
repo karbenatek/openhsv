25ad3068e68135591dd42e139082f6dfe16a9153
Antonin Lindner <54866777+karbenatek@users.noreply.github.com>
2023-03-20 14:26:26 +0100

    Replacing module to import pad class
    The module to import pad class is replaced from skimage.util to numpy.
    More info here: https://discuss.scientific-python.org/t/from-skimage-util-import-pad/308

--- openhsv/analysis/nn.py
+++ openhsv/analysis/nn.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QDialog, QPushButton
-from skimage.util import pad
+from numpy import pad
 import pyqtgraph as pg
 from skimage.color import rgb2gray
 import numpy as np
